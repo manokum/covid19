f4d7421971e3b9a71ae0f0b50825102fad86293c
manokum <34030298+manokum@users.noreply.github.com>
2020-04-27 22:31:41 +0530

    Add files via upload

---
+++ src/components/Cases/CasesByRate.jsx
@@ -0,0 +1,34 @@
+// import React, { useState, useEffect } from 'react';
+// import { NativeSelect, FormControl } from '@material-ui/core';
+
+// // import { fetchCountries } from '../../api';
+// // import styles from './CountryPicker.module.css';
+
+// // const CasesByRate = ({ data }) => {
+// //   const [countries] = useState([]);
+// //   console.log("data CasesByRate",data)
+// //   useEffect(() => {
+// //     // const fetchAPI = async () => {
+// //     //   setCountries(await fetchCountries());
+// //     // };
+// //     // fetchAPI();
+// //   }, []);
+
+// //   return (
+// //     // <FormControl className={styles.formControl}>
+// //     //   <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+// //     //     <option value="">Global</option>
+// //     //     {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
+// //     //   </NativeSelect>
+// //     // </FormControl>
+// //     <p>Test</p>
+// //   );
+// // };
+
+
+// const CasesByRate = ({ data: { confirmed, recovered, deaths } }) => {
+// const barChart = {data};
+//         console.log("barChart",{data})
+//   };
+
+// export default CasesByRate;
