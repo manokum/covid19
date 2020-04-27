ce0777afe85259774b9764bbf566af7972e7378d
Manoranjan <manoranjank@juniper.net>
2020-04-27 22:41:18 +0530

    Updates

---
+++ precache-manifest.8d87b0a21d14768d7196fa0eb2f9ab64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae0f4f9d908b553b60c50159a6f3b1ed",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "977b2bdb0159b3547d87",
+    "url": "/covid19/static/css/main.54e14c55.chunk.css"
+  },
+  {
+    "revision": "3dc3195f5a1620e45f61",
+    "url": "/covid19/static/js/2.8e9bf755.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.8e9bf755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "977b2bdb0159b3547d87",
+    "url": "/covid19/static/js/main.9290e0d2.chunk.js"
+  },
+  {
+    "revision": "3a3760bcf5f8a3cd4d98",
+    "url": "/covid19/static/js/runtime-main.89aefda5.js"
+  },
+  {
+    "revision": "2ce01c11f0776044edbd6e9db07774cf",
+    "url": "/covid19/static/media/image.2ce01c11.jpg"
+  }
+]);
