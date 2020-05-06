6864405b393abb0b7c881d462e1954369c9732fc
Manoranjan <manoranjank@juniper.net>
2020-05-06 17:47:57 +0530

    Changes in the rate card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -13,7 +13,7 @@ const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
   
   return (
     <div className={styles.container}>
-      <Grid container spacing={3} justify="center">
+      {/* <Grid container spacing={3} justify="center">
       
         <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.infected)}>
           <CardContent>
@@ -25,9 +25,7 @@ const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
               
             </Typography>
            
-            {/* <Typography color="textSecondary">
-              {new Date(lastUpdate).toDateString()}
-            </Typography> */}
+          
             <Typography variant="body2" component="p">
               No of active cases
             </Typography>
@@ -36,15 +34,13 @@ const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
         <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.recovered)}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
-              {/* <span>Recovered : {Number(((recovered.value)/(confirmed.value)*100)).toFixed(0) }%</span> */}
+            
               Recovered : <span className={(Number(((recovered.value)/(confirmed.value)*100)).toFixed(0))  > 25 ? styles.class3 : styles.class1 }>{Number(((recovered.value)/(confirmed.value)*100)).toFixed(0) }%</span>
             </Typography>
             <Typography variant="h5" component="h2">
               <CountUp start={0} end={recovered.value} duration={1} separator="," />
             </Typography>
-            {/* <Typography color="textSecondary">
-              {new Date(lastUpdate).toDateString()}
-            </Typography> */}
+            
             <Typography>
             
             </Typography>
@@ -62,9 +58,7 @@ const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
             <Typography variant="h5" component="h2">
               <CountUp start={0} end={deaths.value} duration={1} separator="," />
             </Typography>
-            {/* <Typography color="textSecondary">
-              {new Date(lastUpdate).toDateString()}
-            </Typography> */}
+           
             <Typography variant="body2" component="p">
               Number of deaths COVID19.   
             </Typography>
@@ -81,7 +75,35 @@ const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
             </CardContent>
         </Grid>
         
-      </Grid>
+      </Grid> */}
+
+<table className={styles.customers}>
+<caption className = {styles.flashColor}>Last Updated : {new Date(lastUpdate).toDateString()} {new Date(lastUpdate).toLocaleTimeString()} </caption>
+      {/* <caption>{new Date(lastUpdate).toLocaleTimeString()}</caption> */}
+      <thead>
+        <tr>
+         <th>Confirmed</th>
+          <th>Recovered</th>
+          <th>Recovered Rate</th>
+          <th>Deaths</th>
+          <th>Deaths Rate</th>
+          
+        </tr>
+      </thead>
+      <tbody>
+        
+          <tr>
+            <td>{confirmed.value}</td>
+            <td>{recovered.value}</td>
+            <td><span className={(Number(((recovered.value)/(confirmed.value)*100)).toFixed(0))  > 25 ? styles.class3 : styles.class1 }>{Number(((recovered.value)/(confirmed.value)*100)).toFixed(0) }%</span></td>
+            <td>{deaths.value}</td>
+            <td><span className={(Number(((deaths.value)/(confirmed.value)*100)).toFixed(0))  > 5 ? styles.class1 : styles.class2 }>{Number(((deaths.value)/(confirmed.value)*100)).toFixed(0) }%</span></td>
+          </tr>
+    
+      </tbody>
+    </table> 
+
+
     </div>
   );
 };
---
+++ src/components/IndiaState/IndiaByState.jsx
@@ -0,0 +1,147 @@
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+import { Line, Polar, HorizontalBar,Bubble,Pie,Bar } from 'react-chartjs-2';
+import { indiaStateValue } from '../../api';
+
+import styles from './IndiaByState.module.css';
+
+const IndiaByState = ({ data }) => {
+  
+  // const [state,setCountries] = useState([]);
+  // console.log("data IndiaByState",[state])
+
+  
+  // useEffect(() => {
+  //   const fetchAPI = async () => {
+  //     setCountries(await indiaStateValue());
+  //   };
+  //   fetchAPI();
+  const [firstName, setFirstName] = React.useState(null);
+  const [lastName, setLastName] = React.useState(null);
+  const [allStateData, setallStateData] = React.useState(null);
+  
+  React.useEffect(() => {
+    fetch('https://api.covid19india.org/state_district_wise.json')
+      .then(results => results.json())
+      .then(data => {
+        console.log("data",data)
+        window.data= data;
+         const [name] = [data];
+         function filterTypes(accepted) {
+      
+          for (var type in window.data)
+              if (accepted.indexOf(type) > -1) 
+              window.result[type] = window.data[type];
+          console.log("result", window.result);
+      }
+  
+      // console.log("const [name] = [data];",[name])
+      window.result = {}  
+  
+  console.log("widnow Data",window.data)
+  const allSt = Object.keys(window.data);
+  console.log("widnow allSt",allSt)
+  setallStateData(allSt);
+  filterTypes(['Assam']);
+  console.log("window.result",window.result)
+  const test  = Object.values(window.result);
+  console.log("window.result['key'].districtData",test);
+  const test1 = test[0].districtData;
+  console.log("test1",test1)
+  Object.keys(test1);
+  const test2 = Object.keys(test1);
+  const test3 = Object.entries(test1);
+  console.log("test2",test2)
+  console.log("test3",test3)
+     setFirstName(test3);
+     setLastName(test2);
+        
+      });
+
+
+
+  }, []);
+
+  // if(!state) {
+   
+  //   window.result = {};
+  //   function filterTypes(accepted) {
+      
+  //     for (var type in state)
+  //         if (accepted.indexOf(type) > -1) 
+  //         window.result[type] = state[type];
+  //     console.log("result", window.result);
+  // }
+  // filterTypes(['Maharashtra']);
+
+  // }
+  function handleCountryChange(e){
+    console.log("inside handleCountryChange",e)
+    window.result = {};
+    for (var type in window.data)
+    if (e.indexOf(type) > -1) 
+    window.result[type] = window.data[type];
+    console.log("widnow Data",window.data)
+    const allSt = Object.keys(window.data);
+    console.log("widnow allSt",allSt)
+    setallStateData(allSt);
+    //filterTypes(['Jharkhand']);
+    
+    console.log("window.result",window.result)
+    const test  = Object.values(window.result);
+    console.log("window.result['key'].districtData",test);
+    const test1 = test[0].districtData;
+    console.log("test1",test1)
+    Object.keys(test1);
+    const test2 = Object.keys(test1);
+    const test3 = Object.entries(test1);
+    console.log("test2",test2)
+    console.log("test3",test3)
+       setFirstName(test3);
+       setLastName(test2);
+    
+  }
+  return (
+    <div className={styles.container}>
+      {!firstName || !lastName  || !allStateData ? 'Loading...' : 
+       <div>
+        <FormControl className={styles.formControl}>
+      <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+        {/* <option value="">Assam</option> */}
+        {allStateData.map((product, i) => <option key={i} value={product}>{product}</option>)}
+      </NativeSelect>
+    </FormControl>
+     
+      <table className={styles.customers}>
+      <caption>All States Data</caption>
+      <thead>
+        <tr>
+         <th>District</th>
+          <th>Confirmed</th>
+          <th>Active</th>
+          <th>Deaths</th>
+          <th>Recovered</th>
+        </tr>
+      </thead>
+      <tbody>
+        {firstName.map((product,i) => (
+          <tr key={i}>
+            <td>{product[0]}</td>
+            <td className={(product[1].confirmed)  > 25 ? styles.class3 : styles.class1 }>{product[1].confirmed}</td>
+            <td>{product[1].active}</td>
+            <td>{product[1].deceased}</td>
+            <td>{product[1].recovered}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table> </div>}
+    </div>
+    
+
+  );
+};
+
+
+
+
+export default IndiaByState;
