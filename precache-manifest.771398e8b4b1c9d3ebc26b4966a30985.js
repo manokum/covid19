b43c5317f4fddea31ebaecc44b21ad50eeb2d254
Mano <manor137@in.ibm.com>
2020-05-11 00:05:26 +0530

    Updates

--- precache-manifest.771398e8b4b1c9d3ebc26b4966a30985.js
+++ precache-manifest.771398e8b4b1c9d3ebc26b4966a30985.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e81e15c4dc408530edc2d31c7c7a91f4",
+    "revision": "dc5fc26df575351ee8a863a4d244620e",
     "url": "/covid19/index.html"
   },
   {
-    "revision": "8ba2cadfa4677c5f0225",
-    "url": "/covid19/static/css/main.ef72c633.chunk.css"
+    "revision": "d5972f00355d93adca73",
+    "url": "/covid19/static/css/main.567a073f.chunk.css"
   },
   {
     "revision": "046862755151492f75aa",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid19/static/js/2.23ecb26b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8ba2cadfa4677c5f0225",
-    "url": "/covid19/static/js/main.692ca363.chunk.js"
+    "revision": "d5972f00355d93adca73",
+    "url": "/covid19/static/js/main.3488b1a7.chunk.js"
   },
   {
     "revision": "3a3760bcf5f8a3cd4d98",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19/precache-manifest.786379448db596a00dd043ac72924ac2.js"
+  "/covid19/precache-manifest.771398e8b4b1c9d3ebc26b4966a30985.js"
 );
 
 self.addEventListener('message', (event) => {
